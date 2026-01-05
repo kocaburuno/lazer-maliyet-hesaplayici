75536126eb502b2044fd259d6adfc0f5d78c0c6f
kocaburuno <78270762+kocaburuno@users.noreply.github.com>
2026-01-06 02:17:25 +0300

    Update materials.py

--- materials.py
+++ materials.py
@@ -1,48 +1,71 @@
 # materials.py - Malzeme Verileri ve Sabitler
 
 # --- SABİT PARAMETRELER ---
-DK_UCRETI = 25.0        # Dakika Başına Kesim Ücreti
-PIERCING_SURESI = 2.0   # Patlatma Süresi (Saniye)
+DK_UCRETI = 35.0        # Dakika Başına Kesim Ücreti
+PIERCING_SURESI = 3.0   # Patlatma Süresi (Saniye)
 FIRE_ORANI = 1.15       # Fire Oranı (%15)
 KDV_ORANI = 1.20        # KDV (%20)
 
 # --- VARSAYILAN KG FİYATLARI ---
-# İsim değişikliği burada yapıldı
 VARSAYILAN_FIYATLAR = {
-    "DKP / HRP(Siyah Sac)": 30.0,
+    "DKP / HRP(Siyah Sac)": 33.0,
     "Paslanmaz": 150.0,
     "Alüminyum": 220.0
 }
 
 # --- MALZEME VERİTABANI ---
 VERİ = {
-    # ANAHTAR İSMİ DEĞİŞTİRİLDİ
     "DKP / HRP(Siyah Sac)": {
-        "ozkutle": 7.85, 
+        "ozkutle": 8.0, 
         "kalinliklar": [0.8, 1, 1.2, 1.5, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20], 
-        # Kalınlık (mm): Hız (mm/dk)
         "hizlar": {
-            0.8: 6000, 
-            3: 2800, 
-            10: 800
+            0.8: 6000,
+            1: 6000,
+            1.2: 6000,
+            1.5: 6000,
+            2: 5000,
+            3: 3750,
+            4: 3000,
+            5: 2750,
+            6: 2250,
+            8: 1750,
+            10: 1500,
+            12: 1200,
+            15: 1900,
+            20: 600
         }
     },
     "Paslanmaz": {
         "ozkutle": 8.0, 
-        "kalinliklar": [0.8, 1, 1.2, 1.5, 2, 3, 4, 5, 6, 8, 10, 12, 15], 
+        "kalinliklar": [0.8, 1, 1.2, 1.5, 2, 3, 4, 5, 6, 8, 10], 
         "hizlar": {
-            0.8: 7000, 
-            2: 4500, 
-            10: 500
+            0.8: 8000,
+            1: 7000,
+            1.2: 6600,
+            1.5: 6000,
+            2: 5000,
+            3: 4500,
+            4: 4000,
+            5: 3000,
+            6: 2000,
+            8: 1000,
+            10: 750
         }
     },
     "Alüminyum": {
         "ozkutle": 2.7, 
         "kalinliklar": [0.8, 1, 1.2, 1.5, 2, 3, 4, 5, 6, 8], 
         "hizlar": {
-            0.8: 8000, 
-            2: 5000, 
-            8: 600
+            0.8: 7000,
+            1: 6500,
+            1.2: 6000,
+            1.5: 5500,
+            2: 5000,
+            3: 4500,
+            4: 4000,
+            5: 3000,
+            6: 2000,
+            8: 1000
         }
     }
 }
