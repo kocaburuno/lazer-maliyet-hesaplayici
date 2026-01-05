0b43d94f97f1df98eefcfd4d16b77b758b5e906e
kocaburuno <78270762+kocaburuno@users.noreply.github.com>
2026-01-06 02:00:37 +0300

    Update materials.py

--- materials.py
+++ materials.py
@@ -7,16 +7,17 @@ FIRE_ORANI = 1.15       # Fire Oranı (%15)
 KDV_ORANI = 1.20        # KDV (%20)
 
 # --- VARSAYILAN KG FİYATLARI ---
+# İsim değişikliği burada yapıldı
 VARSAYILAN_FIYATLAR = {
-    "Siyah Sac": 30.0,
+    "DKP / HRP(Siyah Sac)": 30.0,
     "Paslanmaz": 150.0,
     "Alüminyum": 220.0
 }
 
 # --- MALZEME VERİTABANI ---
-# Buraya yeni kalınlıklar veya hızlar ekleyebilirsin.
 VERİ = {
-    "Siyah Sac": {
+    # ANAHTAR İSMİ DEĞİŞTİRİLDİ
+    "DKP / HRP(Siyah Sac)": {
         "ozkutle": 7.85, 
         "kalinliklar": [0.8, 1, 1.2, 1.5, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20], 
         # Kalınlık (mm): Hız (mm/dk)
